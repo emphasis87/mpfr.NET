094421de3702970bebfa941141807f9ebdc23506
Luboš Hemala <lubos.hemala@gmail.com>
2016-08-06 22:52:36 +0200

    #1 new mpfr.h header, fixed comments, Pow, Dim, arithmentic and some special functions' tests

--- src/mpfrNET.Tests/ArithmeticFunctionsTests.cs
+++ src/mpfrNET.Tests/ArithmeticFunctionsTests.cs
@@ -7,51 +7,151 @@ namespace mpfrNET.Tests
 {
 	public class ArithmeticFunctionsTests
 	{
-		[Test]
-		public void Can_Add()
+		[TestCase(0, 0)]
+		[TestCase(1.1, -1.1)]
+		[TestCase(3.0123456789012345, -3.0123456789012345)]
+		[TestCase(-3.0123456789012345, 3.0123456789012345)]
+		public void Can_Neg(double left, double result)
 		{
-			var x1 = new BigDecimal(1.1);
-			x1.Add(new BigDecimal(2.123456789));
+			var x = (double)new BigDecimal(left).Neg();
+			x.Should().Be(result);
+		}
 
-			((double)x1).Should().Be(3.223456789);
+		[TestCase(0, 0)]
+		[TestCase(-1.1, 1.1)]
+		[TestCase(1.1, 1.1)]
+		[TestCase(3.0123456789012345, 3.0123456789012345)]
+		[TestCase(-3.0123456789012345, 3.0123456789012345)]
+		public void Can_Abs(double left, double result)
+		{
+			var x = (double)new BigDecimal(left).Abs();
+			x.Should().Be(result);
+		}
 
-			x1.Add(new BigDecimal(0.0000000001234567));
+		[TestCase(0, 0, 0)]
+		[TestCase(0, 1, 1)]
+		[TestCase(-1, 1, 0)]
+		[TestCase(1.1, 2.123456789, 3.223456789)]
+		[TestCase(1.1, 2.1234567890123456, 3.2234567890123456)]
+		[TestCase(1.123456789123456, 2.876543210876544, 4.0)]
+		public void Can_Add(double left, double right, double result)
+		{
+			var x = (double)new BigDecimal(left).Add(new BigDecimal(right));
+			x.Should().Be(result);
+		}
 
-			((double)x1).Should().Be(3.2234567891234567);
+		[TestCase(0, 0, 0)]
+		[TestCase(0, 1, -1)]
+		[TestCase(1, 0, 1)]
+		[TestCase(1.1, 0.1, 1)]
+		[TestCase(3.0123456789012345, 1, 2.0123456789012345)]
+		public void Can_Sub(double left, double right, double result)
+		{
+			var x = (double)new BigDecimal(left).Sub(new BigDecimal(right));
+			x.Should().Be(result);
+		}
 
-			x1.Add(new BigDecimal(0.000000000000000009));
+		[TestCase(0, 1, 0)]
+		[TestCase(1, 1, 1)]
+		[TestCase(1, -1, -1)]
+		[TestCase(3, 1, 3)]
+		[TestCase(0.3333333333333333, 3, 1)]
+		public void Can_Mul(double left, double right, double result)
+		{
+			var x = (double)new BigDecimal(left).Mul(new BigDecimal(right));
+			x.Should().Be(result);
+		}
 
-			((double)x1).Should().Be(3.2234567891234567);
+		[TestCase(0, 1, 0)]
+		[TestCase(1, 1, 1)]
+		[TestCase(1, -1, -1)]
+		[TestCase(3, 1, 3)]
+		[TestCase(1, 3, 0.3333333333333333)]
+		public void Can_Div(double left, double right, double result)
+		{
+			var x = (double)new BigDecimal(left).Div(new BigDecimal(right));
+			x.Should().Be(result);
+		}
+
+		[TestCase(0, 0, 1)]
+		[TestCase(0, 1, 0)]
+		[TestCase(1, 0, 1)]
+		[TestCase(1, 1, 1)]
+		[TestCase(-1, 2, 1)]
+		[TestCase(-1, 3, -1)]
+		[TestCase(10, 2, 100)]
+		[TestCase(2, 10, 1024)]
+		[TestCase(4, -2, 0.0625)]
+		[TestCase(3.3, 3.3, 51.41572944406660529715428336039799250538522979598295133355291551603310643)]
+		[TestCase(4, 1.25, 5.656854249492380195206754896838792314278687501507792292706718951962929913)]
+		public void Can_Pow(double left, double right, double result)
+		{
+			var x = (double)new BigDecimal(left, 1024).Pow(new BigDecimal(right, 1024));
+			x.Should().Be(result);
+		}
+
+		[TestCase(1, 1)]
+		[TestCase(2, 1.414213562373095048801688724209698078569671875376948073176679737990732478)]
+		[TestCase(6.1, 2.469817807045693805907099461628775179194144925820212858228439090302867211)]
+		public void Can_Sqrt(double left, double result)
+		{
+			var x = (double)new BigDecimal(left).Sqrt();
+			x.Should().Be(result);
+		}
+
+		[TestCase(1, 1)]
+		[TestCase(4, 0.5)]
+		[TestCase(3, 0.577350269189625764509148780501957455647601751270126876018602326483977672)]
+		[TestCase(4.25, 0.485071250072665947037812924232244355899670497102778874635133361540582864)]
+		public void Can_RecSqrt(double left, double result)
+		{
+			var x = (double)new BigDecimal(left).RecSqrt();
+			x.Should().Be(result);
+		}
+
+		[TestCase(1, 1)]
+		[TestCase(8, 2)]
+		[TestCase(3, 1.442249570307408382321638310780109588391869253499350577546416194541687596)]
+		[TestCase(10.25, 2.172240742884305950861175273027287963596802126904442410000785269899687397)]
+		public void Can_Cbrt(double left, double result)
+		{
+			var x = (double)new BigDecimal(left).Cbrt();
+			x.Should().Be(result);
+		}
+
+		[TestCase(1, 1, 1)]
+		[TestCase(-27, 3, -3)]
+		[TestCase(1024, 10, 2)]
+		[TestCase(3, 7, 1.169930812758686886462975725513734667699404196420934209030218965589333936)]
+		public void Can_Root(double left, int right, double result)
+		{
+			var x = (double)new BigDecimal(left).Root((ulong)right);
+			x.Should().Be(result);
+		}
+
+		[TestCase(0, 0, 0)]
+		[TestCase(1, 1, 0)]
+		[TestCase(1, 2, 0)]
+		[TestCase(3, 1, 2)]
+		[TestCase(10, 1, 9)]
+		[TestCase(-1, -2, 1)]
+		public void Can_Dim(double left, double right, double result)
+		{
+			var x = (double)new BigDecimal(left).Dim(new BigDecimal(right));
+			x.Should().Be(result);
 		}
 
 		[Test]
-		public void When_passed_null_Add_should_throw()
+		public void Binary_functions_should_throw_for_null_argument()
 		{
 			var x1 = new BigDecimal(1.1);
 
-			Action act = () => x1.Add(null);
-
-			act.ShouldThrow<NullReferenceException>();
-		}
-
-		[Test]
-		public void Can_Sub()
-		{
-			var x1 = new BigDecimal(3.012345678901234567890123456789);
-
-			x1.Sub(new BigDecimal(1));
-
-			((double)x1).Should().Be(2.0123456789012345);
-		}
-
-		[Test]
-		public void When_passed_null_Sub_should_throw()
-		{
-			var x1 = new BigDecimal(1.1);
-
-			Action act = () => x1.Sub(null);
-
-			act.ShouldThrow<NullReferenceException>();
+			((Action)(() => x1.Add(null))).ShouldThrow<NullReferenceException>();
+			((Action)(() => x1.Sub(null))).ShouldThrow<NullReferenceException>();
+			((Action)(() => x1.Mul(null))).ShouldThrow<NullReferenceException>();
+			((Action)(() => x1.Div(null))).ShouldThrow<NullReferenceException>();
+			((Action)(() => x1.Pow(null))).ShouldThrow<NullReferenceException>();
+			((Action)(() => x1.Dim(null))).ShouldThrow<NullReferenceException>();
 		}
 	}
 }
---
+++ src/mpfrNET.Tests/SpecialFunctionsTests.cs
@@ -0,0 +1,96 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.ArbitraryPrecision;
+
+namespace mpfrNET.Tests
+{
+	public class SpecialFunctionsTests
+	{
+		[TestCase(double.NaN, double.NaN)]
+		[TestCase(double.NegativeInfinity, double.NaN)]
+		[TestCase(double.PositiveInfinity, double.PositiveInfinity)]
+		[TestCase(0, double.NegativeInfinity)]
+		[TestCase(1, 0)]
+		[TestCase(-1, double.NaN)]
+		[TestCase(0.4, -0.916290731874155065183527211768011071450101219908262467791967881980785365)]
+		[TestCase(2, 0.693147180559945309417232121458176568075500134360255254120680009493393621)]
+		[TestCase(9.999, 2.302485087993712325682657954670077243204266090894201900519238035204856264)]
+		public void Can_Ln(double left, double result)
+		{
+			var x = (double)new BigDecimal(left).Ln();
+			x.Should().Be(result);
+		}
+
+		[TestCase(double.NaN, double.NaN)]
+		[TestCase(double.NegativeInfinity, double.NaN)]
+		[TestCase(double.PositiveInfinity, double.PositiveInfinity)]
+		[TestCase(0, double.NegativeInfinity)]
+		[TestCase(1, 0)]
+		[TestCase(-1, double.NaN)]
+		[TestCase(0.4, -1.321928094887362347870319429489390175864831393024580612054756395815934776)]
+		[TestCase(1024, 10)]
+		[TestCase(9.999, 3.321783818169317312667541759643564948423217700960905620325686812784034478)]
+		public void Can_Log2(double left, double result)
+		{
+			var x = (double)new BigDecimal(left).Log2();
+			x.Should().Be(result);
+		}
+
+		[TestCase(double.NaN, double.NaN)]
+		[TestCase(double.NegativeInfinity, double.NaN)]
+		[TestCase(double.PositiveInfinity, double.PositiveInfinity)]
+		[TestCase(0, double.NegativeInfinity)]
+		[TestCase(1, 0)]
+		[TestCase(-1, double.NaN)]
+		[TestCase(0.4, -0.397940008672037609572522210551013946463620237075782917379145077745783621)]
+		[TestCase(1000, 3)]
+		[TestCase(9.999, 0.999956568380192489615443955976192773326249274054297415662088936238659348)]
+		public void Can_Log10(double left, double result)
+		{
+			var x = (double)new BigDecimal(left).Log10();
+			x.Should().Be(result);
+		}
+
+		[TestCase(double.NaN, double.NaN)]
+		[TestCase(double.NegativeInfinity, 0)]
+		[TestCase(double.PositiveInfinity, double.PositiveInfinity)]
+		[TestCase(0, 1)]
+		[TestCase(1, 2.718281828459045235360287471352662497757247093699959574966967627724076630)]
+		[TestCase(-2, 0.135335283236612691893999494972484403407631545909575881468158872654073374)]
+		[TestCase(4.999, 148.2648201253242342339591225627208985720458425149174668922941623858720050)]
+		[TestCase(9.999, 22004.45033857464715721623840438148225727827858565599427258232680618244485)]
+		public void Can_Exp(double left, double result)
+		{
+			var x = (double)new BigDecimal(left).Exp();
+			x.Should().Be(result);
+		}
+
+		[TestCase(double.NaN, double.NaN)]
+		[TestCase(double.NegativeInfinity, 0)]
+		[TestCase(double.PositiveInfinity, double.PositiveInfinity)]
+		[TestCase(0, 1)]
+		[TestCase(1, 2)]
+		[TestCase(-2, 0.25)]
+		[TestCase(4.999, 31.97782697569448070151613937163205819747369486211219520368146244813466876)]
+		[TestCase(9.999, 1023.290463222223382448516459892225862319158235587590246517806798340309400)]
+		public void Can_Exp2(double left, double result)
+		{
+			var x = (double)new BigDecimal(left).Exp2();
+			x.Should().Be(result);
+		}
+
+		[TestCase(double.NaN, double.NaN)]
+		[TestCase(double.NegativeInfinity, 0)]
+		[TestCase(double.PositiveInfinity, double.PositiveInfinity)]
+		[TestCase(0, 1)]
+		[TestCase(1, 10)]
+		[TestCase(-2, 0.01)]
+		[TestCase(4.999, 99770.00638225533171944219428537623105521186139457315462487823089094547653)]
+		[TestCase(-3.999, 0.000100230523807789967191540488932811055405366845354216064641163485230474)]
+		public void Can_Exp10(double left, double result)
+		{
+			var x = (double)new BigDecimal(left).Exp10();
+			x.Should().Be(result);
+		}
+	}
+}
